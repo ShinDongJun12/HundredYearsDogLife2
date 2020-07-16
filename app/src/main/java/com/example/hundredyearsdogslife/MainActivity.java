ed5b1b1a6fd6c844121a45134db8c2f05bb397d1
username <webserver3315@naver.com>
2020-07-16 17:05:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hundredyearsdogslife/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hundredyearsdogslife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro); // 제일 처음에 보이는 화면.
+    }
+}
